9d43281f334f97f20733fa2543252ba3ecef44cd
Balazsoficial <193341121+Balazsoficial@users.noreply.github.com>
2025-01-11 20:31:22 +0100

    Added basic redness check for future use, changed isEven to bool added sleep to the game

--- casino.cpp
+++ casino.cpp
@@ -12,12 +12,13 @@ using namespace ::std;
 string GetTempFolder();
 int Getmoney();
 int Setmoney(int money);
-void HandleBetingNumbers(int bet,int randomNumber,int guess);
+void HandleBettingNumbers(int bet,int randomNumber,int guess);
 void SetColor(int textColor, int backgroundColor);
-void IsEven (int Number);
+bool IsEven (int Number);
 void HandleBettingEo(int bet,int randomNum,char guess);
 void Victory();
 void Loss();
+bool IsRed(int randomNum);
 int main()
 {
     while (true)
@@ -43,7 +44,7 @@ int main()
     int money =Getmoney();
     int type;
     int randomNumber =1+(rand()%36);
-        cout  << "What do you want to play\n[+] 1. Select a number between 1 and 36!\n[+] 2. Even or odd! Enter your choiche below: ";
+        cout  << "What do you want to play\n▶ 1. Select a number between 1 and 36!\n▶ 2. Even or odd!\nEnter your choice below: ";
         cin >> type;
     if (type == 1)
         {
@@ -62,7 +63,7 @@ int main()
         cout << "Make a bet: ";
         cin >> bet;
     }
-    HandleBetingNumbers(bet,randomNumber,guess);
+    HandleBettingNumbers(bet,randomNumber,guess);
 
     if(guess == randomNumber) {
         Victory();
@@ -73,6 +74,7 @@ int main()
           cout << "The correct number was "<< randomNumber<<"!\n";
         }
         cout << "press ctrl+c if you want to quit!\n";
+        _sleep(1000);
 
     }
     if (type == 2) {
@@ -92,7 +94,7 @@ int main()
             cin >> bet;
         }
         HandleBettingEo(bet,randomNumber,eochoiche);
-        if(randomNumber%2==0 && eochoiche=='e' ||randomNumber%2==1 && eochoiche=='o') {
+        if(randomNumber%2==0 && std::tolower(eochoiche)=='e' ||randomNumber%2==1 && std::tolower(eochoiche)=='o') {
             Victory();
             cout << "You guessed correctly, the number was: "<< randomNumber<<"!\n";
         }
@@ -136,7 +138,7 @@ int Setmoney(int money) {
     storage.close();
     return 0;
 }
-void HandleBetingNumbers(int bet,int randomNumber,int guess) {
+void HandleBettingNumbers(int bet,int randomNumber,int guess) {
     int money = Getmoney();
     if (randomNumber!=guess) {
         money = money-bet;
@@ -155,17 +157,17 @@ void SetColor(int textColor, int backgroundColor) {
     HANDLE consoleHandle = GetStdHandle(STD_OUTPUT_HANDLE);
     SetConsoleTextAttribute(consoleHandle, textColor);
 }
-void IsEven(int Number) {
+bool IsEven(int Number) {
     if (Number % 2 == 0) {
-        true;
+        return true;
     }
     else {
-        false;
+       return false;
     }
 }
 void HandleBettingEo(int bet,int randomNumber,char guess) {
     int money = Getmoney();
-    if (randomNumber%2==0 && guess=='o' ||randomNumber%2==1 && guess=='e') {
+    if (randomNumber%2==0 && std::tolower(guess)=='o' ||randomNumber%2==1 && std::tolower(guess)=='e') {
         money = money-bet;
         Setmoney(money);
     }
@@ -194,3 +196,45 @@ void Loss() {
      cout <<"   ██║   ╚██████╔╝╚██████╔╝    ███████╗╚██████╔╝███████║   ██║   ██╗"<<endl;
      cout <<"   ╚═╝    ╚═════╝  ╚═════╝     ╚══════╝ ╚═════╝ ╚══════╝   ╚═╝   ╚═╝"<<endl;
 }
+bool IsRed(int randomNum) {
+    switch (randomNum) {
+        case 1: return true;
+        case 2: return false;
+        case 3: return true;
+        case 4: return false;
+        case 5: return true;
+        case 6: return false;
+        case 7: return true;
+        case 8: return false;
+        case 9: return true;
+        case 10: return false;
+        case 11: return false;
+        case 12: return true;
+        case 13: return false;
+        case 14: return true;
+        case 15: return false;
+        case 16: return true;
+        case 17: return false;
+        case 18: return true;
+        case 19: return true;
+        case 20: return false;
+        case 21: return true;
+        case 22: return false;
+        case 23: return true;
+        case 24: return false;
+        case 25: return true;
+        case 26: return false;
+        case 27: return true;
+        case 28: return false;
+        case 29: return false;
+        case 30: return true;
+        case 31: return false;
+        case 32: return true;
+        case 33: return false;
+        case 34: return true;
+        case 35: return false;
+        case 36: return true;
+
+    }
+}
+
