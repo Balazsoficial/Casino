09d399236ca8a577a6fab7a9f8992c645aba12f7
Balazsoficial <193341121+Balazsoficial@users.noreply.github.com>
2025-01-14 17:10:09 +0100

    Backup while implementing green

--- casino.cpp
+++ casino.cpp
@@ -20,9 +20,7 @@ void Loss();
 bool IsRed(int randomNum);
 void HandleBettingColors(int bet,int randomNum,string guess);
 void GetColor(int RandomNum);
-int main()
-
-{
+int main() {
     SetConsoleOutputCP(CP_UTF8);
     while (true) {
         SetColor(2,15);
@@ -50,19 +48,19 @@ int main()
         int bet;
         int money =Getmoney();
         int type;
-        int randomNumber =1+(rand()%36);
+        int randomNumber =1+(rand()%37);
         cout << "Your balance is :" <<Getmoney()<<"$!\n" << endl;
-        cout  << "What do you want to play\n> 1. Select a number between 1 and 36!\n> 2. Even or odd!\n> 3. Black/Red!\nEnter your choice below: ";
+        cout  << "What do you want to play\n> 1. Select a number between 1 and 36!\n> 2. Even or odd!\n> 3. Black/Red!\n4. 0/00 \nEnter your choice below: ";
         cin >> type;
         if (type == 1)
 
         {
 
-           // cout <<"You have "<<Getmoney() <<"$!\n";
+            // cout <<"You have "<<Getmoney() <<"$!\n";
             cout << "Enter Your guess: ";
             cin >> guess;
             while (guess>36 or guess<1) {
-                cout << "Enter Your guess (Between 0-36): ";
+                cout << "Enter Your guess (Between 1-36): ";
                 cin >> guess;
             }
 
@@ -90,7 +88,7 @@ int main()
         }
         if (type == 2) {
             char eochoiche;
-      //      cout <<"You have "<<Getmoney() <<"$!\n";
+            //      cout <<"You have "<<Getmoney() <<"$!\n";
             cout << "Enter Your guess even(e) or odd(o):";
             cin >> eochoiche;
             while (eochoiche!='e' && eochoiche!='o') {
@@ -122,11 +120,11 @@ int main()
         if (type == 3)
         {
             string strquess;
-        //    cout <<"You have "<<Getmoney() <<"$!\n";
-            cout << "Enter Your guess (Red or Black): ";
+            //    cout <<"You have "<<Getmoney() <<"$!\n";
+            cout << "Enter Your guess (Red or Black or Green): ";
             cin >> strquess;
-            while (strquess!="Red" && strquess!="Black") {
-                cout << "Enter Your guess (Red or Black): ";
+            while (strquess!="Red" && strquess!="Black" && strquess!= "Green") {
+                cout << "Enter Your guess (Red or Black or Green): ";
                 cin >> strquess;
             }
 
@@ -138,7 +136,9 @@ int main()
                 cin >> bet;
             }
             HandleBettingColors(bet,randomNumber,strquess);
+            if (randomNumber==0||randomNumber==37) {
 
+            }
             if(IsRed(randomNumber) && strquess =="Red"||IsRed(randomNumber)==false && strquess =="Black") {
                 Victory();
                 GetColor(randomNumber);
@@ -152,16 +152,58 @@ int main()
             system("cls");
 
         }
-        if (type == 4) {
+        if (type == 5) {
             cout << "Reseting money file...\n";
-          // filesystem::remove(GetTempFolder());
+            // filesystem::remove(GetTempFolder());
             remove(GetTempFolder().c_str());
             _sleep(500);
             cout << "Successfully reset money file!\n";
             system("cls");
         }
+        if (type == 4)
+        {
+
+            string zeros;
+            cout << "Enter Your guess: ";
+            cin >> zeros;
+            while (zeros!="0" && zeros!="00") {
+                cout << "Enter Your guess zero or double zero: ";
+                cin >> zeros;
+            }
+            if (zeros=="0") {
+                guess = 0;
+            }
+            else if (zeros=="00") {
+                guess = 37;
+            }
+
+            cout << "Make a bet: ";
+            cin >> bet;
+            while (bet>Getmoney()) {
+                cout << "Your bet cant be bigger than your money which you have: "<<Getmoney<<"$!\n";
+                cout << "Make a bet: ";
+                cin >> bet;
+            }
+            HandleBettingNumbers(bet,randomNumber,guess);
+
+            if(guess == randomNumber) {
+                Victory();
+                if (randomNumber == 37)
+                {
+                cout << "The correct number was "<< "00"<<"!\n";}
+            }
+            else{
+                Loss();
+                if (randomNumber == 37){
+                cout << "The correct number was "<< randomNumber<<"!\n";}
+            }
+            cout << "press ctrl+c if you want to quit!\n";
+            _sleep(1000);
+            system("cls");
+
+        }
+        return 0;
     }
-    return 0;
 }
 
 string GetTempFolder() {
